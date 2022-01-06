eec641f3bacd4004d292955b42282696215ca5b1
Timur Kasiev <timurkasievsochi@gmail.com>
2022-01-06 21:44:54 +0300

    Add ModifierKeys constant for keyboard events

---
+++ src/constants/keyboard.tsx
@@ -0,0 +1,6 @@
+export const ModifierKeys = {
+    ALT: { name: 'Alt', eventName: 'altKey' },
+    CONTROL: { name: 'Control', eventName: 'ctrlKey' },
+    META: { name: 'Meta', eventName: 'metaKey' },
+    SHIFT: { name: 'Shift', eventName: 'shiftKey' },
+} as const;
