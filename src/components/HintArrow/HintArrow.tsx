3e20213256dc50c6708fd87bb8dc1f14dc7d6ec7
Timur Kasiev <timurkasievsochi@gmail.com>
2022-01-27 14:18:58 +0300

    Make suggestion arrow prettier

--- src/components/HintArrow/HintArrow.tsx
+++ src/components/HintArrow/HintArrow.tsx
@@ -1,19 +1,37 @@
+// TODO: make calculations relative to the parent element
+
 type HintArrowProps = {
     fromElement: HTMLElement;
     toElement: HTMLElement;
 };
 
-export const HintArrow: React.FC<HintArrowProps> = ({
-    fromElement,
-    toElement,
-}) => {
-    const { x: fromX, y: fromY, width: fromWidth, height: fromHeight } = fromElement.getBoundingClientRect();
-    const { x: toX, y: toY, width: toWidth, height: toHeight } = toElement.getBoundingClientRect();
+const getElemCoordsRelativeToParent = (
+    element: HTMLElement
+) => ({
+    x: element.offsetLeft,
+    y: element.offsetTop,
+    width: element.offsetWidth,
+    height: element.offsetHeight,
+});
+
+export const HintArrow: React.FC<HintArrowProps> = ({ fromElement, toElement }) => {
+    const {
+        x: fromX,
+        y: fromY,
+        width: fromWidth,
+        height: fromHeight,
+    } = getElemCoordsRelativeToParent(fromElement);
+    const {
+        x: toX,
+        y: toY,
+        width: toWidth,
+        height: toHeight,
+    } = getElemCoordsRelativeToParent(toElement);
 
     const arrowCoordProps = {
-        x1: fromX - fromWidth * 1.5,
+        x1: fromX + fromWidth * 0.5,
         y1: fromY + fromHeight * 0.5,
-        x2: toX - toWidth * 1.5,
+        x2: toX + toWidth * 0.5,
         y2: toY + toHeight * 0.5,
     };
 
@@ -26,8 +44,8 @@ export const HintArrow: React.FC<HintArrowProps> = ({
                     viewBox="0 -5 10 10"
                     refX="5"
                     refY="0"
-                    markerWidth="5"
-                    markerHeight="5"
+                    markerWidth="4"
+                    markerHeight="4"
                     orient="auto"
                 >
                     <path d="M0,-5L10,0L0,5"></path>
@@ -36,7 +54,7 @@ export const HintArrow: React.FC<HintArrowProps> = ({
             <line
                 {...arrowCoordProps}
                 className="hint-arrow__line"
-                strokeWidth="4"
+                strokeWidth="3"
                 markerEnd="url(#arrow)"
             ></line>
         </svg>
